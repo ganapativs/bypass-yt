1d5c977ddabb4724cd0e0c1af4040e6f7da5d45a
Ganapati V S <vsg.inbox@gmail.com>
2019-06-24 00:00:19 +0530

    Initial windowing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+const onSubmit = e => {
+  e.preventDefault();
+  const url = document.getElementById("url").value;
+
+  if (url) {
+    const u = new URL(url);
+    const id = u.searchParams.get("v");
+
+    if (id) {
+      window.location.href = `https://www.youtube.com/embed/${id}`;
+    }
+  }
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <img
+        src="/favicon.png"
+        alt="Bypass YT"
+        title="Bypass YT"
+        draggable={false}
+      />
+      <form onSubmit={onSubmit} autoComplete="off">
+        <h4>
+          Enter the{" "}
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://support.google.com/youtube/answer/2802167?hl=en"
+          >
+            age restricted
+          </a>{" "}
+          YouTube URL below
+        </h4>
+        <input
+          className="shadowed"
+          type="url"
+          id="url"
+          pattern="https://.*"
+          size="50"
+          required
+          autoFocus
+          placeholder="https://www.youtube.com/watch?v=PzmFJRRxa7I"
+        />
+        <button className=" shadowed" type="submit">
+          Bypass
+        </button>
+      </form>
+      <p>
+        Time pass stuff by{" "}
         <a
-          className="App-link"
-          href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
+          href="https://twitter.com/ganapativs"
         >
-          Learn React
-        </a>
-      </header>
+          @ganapativs
+        </a>{" "}
+        🎉
+      </p>
     </div>
   );
 }
